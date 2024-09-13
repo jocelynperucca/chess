3bc75df287a273b97ab173a22ee1014d294ac8d0
jocelynperucca <jocelynperucca@gmail.com>
2024-09-12 18:18:27 -0600

    added code to test if could capture enemy for rook. passed rookCaptureEnemy

--- shared/src/main/java/chess/Rook.java
+++ shared/src/main/java/chess/Rook.java
@@ -21,6 +21,7 @@ public class Rook extends ChessPiece {
         for (int[] direction : directions) {
             int rowOffset = direction[0];
             int colOffset = direction[1];
+            boolean stop = false;
 
             for (int i = 1; i <= 8; i++) {
 
@@ -29,6 +30,17 @@ public class Rook extends ChessPiece {
                 if(newRow > 0 && newRow < 9 && newCol > 0 && newCol < 9) {
                     ChessPosition newPosition = new ChessPosition(newRow, newCol);
                     ChessMove newMove = new ChessMove(position, newPosition, null);
+                    if(hasPiece(board, newMove).equals("good")) {
+                        moves.add(newMove);
+                    } else if (hasPiece(board, newMove).equals("can capture")) {
+                        moves.add(newMove);
+                        stop = true;
+                    } else if (hasPiece(board, newMove).equals("same team")) {
+                        stop = true;
+                    }
+                    if (stop) {
+                        break;
+                    }
                     moves.add(newMove);
                 } else {
                     break;
