127d6fd0a5d867e629052bad0784b64b09956f99
jocelynperucca <jocelynperucca@gmail.com>
2024-10-15 11:46:07 -0600

    GameData Creation and authdata I think finished

--- server/src/main/java/Data/AuthData.java
+++ server/src/main/java/Data/AuthData.java
@@ -1,4 +1,22 @@
 package Data;
 
 public class AuthData {
+
+    String username;
+    String authToken;
+
+    public AuthData (String username, String authToken) {
+        this.username = username;
+
+        this.authToken = authToken;
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
 }
