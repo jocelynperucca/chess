c3427d78d046d7743a82e94a7de4990966a7c828
jocelynperucca <jocelynperucca@gmail.com>
2024-10-15 11:54:09 -0600

    game data intializers and getters

--- server/src/main/java/Data/GameData.java
+++ server/src/main/java/Data/GameData.java
@@ -1,4 +1,40 @@
 package Data;
 
+import chess.ChessGame;
+
 public class GameData {
+    int gameID;
+    String whiteUsername;
+    String blackUsername;
+    String gameName;
+    ChessGame game;
+
+    public GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+        this.gameID = gameID;
+        this.whiteUsername = whiteUsername;
+        this.blackUsername = blackUsername;
+        this.gameName = gameName;
+        this.game = game;
+    }
+
+    //getters
+    public int getGameID(){
+        return gameID;
+    }
+
+    public String getWhiteUsername() {
+        return whiteUsername;
+    }
+
+    public String getBlackUsername() {
+        return blackUsername;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public ChessGame getGame() {
+        return game;
+    }
 }
