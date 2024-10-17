56b677ace67e76461a40c964eb342b8c565d0efe
jocelynperucca <jocelynperucca@gmail.com>
2024-10-16 23:03:27 -0600

    setup of authDAO for basic functions i think

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,9 @@
 package dataaccess;
 
 public interface AuthDAO {
+    void saveAuthToken(String authToken) throws DataAccessException;
+
+    void getAuthToken(String authToken) throws DataAccessException;
+
+    void deleteAuthToken(String authToken) throws DataAccessException;
 }
