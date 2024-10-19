20ae1da159d645792e57777b290d08d0c2ac37fb
jocelynperucca <jocelynperucca@gmail.com>
2024-10-18 23:08:21 -0600

    creation of ClearResult and edited parameters for clearService

--- server/src/main/java/Handler/ClearHandler.java
+++ server/src/main/java/Handler/ClearHandler.java
@@ -1,4 +1,45 @@
 package Handler;
 
-public class ClearHandler {
+import dataaccess.DataAccessException;
+import model.*;
+import service.ClearService;
+import service.LogoutService;
+import com.google.gson.Gson;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class ClearHandler implements Route {
+
+    private final ClearService clearService;
+    private final Gson gson = new Gson(); // Initialize Gson here
+
+    public ClearHandler(ClearService clearService) {
+        this.clearService = clearService;
+    }
+
+    @Override
+    public Object handle(Request Request, Response response) throws DataAccessException {
+
+        ClearResult clearResult;
+        try {
+            String authToken = Request.headers("Authorization");
+            clearResult = clearService.clear(authToken);
+            if (clearResult.clearMessage().contains("Cleared")) {
+                response.status(200);
+            } else if (clearResult.clearMessage().contains("Unauthorized")) {
+                response.status(401);
+            }
+
+
+        } catch (DataAccessException e) {
+            clearResult = new ClearResult(e.getMessage());
+            response.status(500);
+
+        }
+
+        response.type("application/json");
+        return gson.toJson(clearResult);
+
+    }
 }
--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,4 +1,25 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
+import model.ClearResult;
+
+
 public class ClearService {
+
+    private final AuthDAO authDao;
+    private final GameDAO gameDao;
+    private final UserDAO userDao;
+
+    public ClearService(AuthDAO authDao, GameDAO gameDao, UserDAO userDao) {
+        this.authDao = authDao;
+        this.gameDao = gameDao;
+        this.userDao = userDao;
+    }
+
+    public ClearResult clear() throws DataAccessException {
+
+    }
 }
