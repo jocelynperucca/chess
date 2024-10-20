579bbe11dae9205b74fe508ec8c25a94ff330ae8
jocelynperucca <jocelynperucca@gmail.com>
2024-10-20 16:13:19 -0600

    Written Register Test works

--- server/src/test/java/service/MemoryDAOTests.java
+++ server/src/test/java/service/MemoryDAOTests.java
@@ -1,6 +1,12 @@
 package service;
 
 import dataaccess.*;
+import model.RegisterRequest;
+import model.RegisterResult;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 
 public class MemoryDAOTests {
 
@@ -8,11 +14,38 @@ public class MemoryDAOTests {
     AuthDAO authDAO;
     GameDAO gameDAO;
 
-    RegisterService registerService = new RegisterService(userDAO, authDAO);
-    LoginService loginService = new LoginService(userDAO, authDAO);
-    LogoutService logoutService = new LogoutService(authDAO);
-    ListGamesService listGamesService = new ListGamesService(authDAO, gameDAO);
-    CreateGameService createGameService = new CreateGameService(authDAO, gameDAO);
-    JoinGameService joinGameService = new JoinGameService(authDAO,gameDAO);
-    ClearService clearService = new ClearService(authDAO, gameDAO, userDAO);
+    RegisterService registerService;
+    LoginService loginService;
+    LogoutService logoutService;
+    ListGamesService listGamesService;
+    CreateGameService createGameService;
+    JoinGameService joinGameService;
+    ClearService clearService;
+
+    @BeforeEach
+    public void start() throws DataAccessException {
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        gameDAO = new MemoryGameDAO();
+
+        registerService = new RegisterService(userDAO, authDAO);
+        loginService = new LoginService(userDAO, authDAO);
+        logoutService = new LogoutService(authDAO);
+        listGamesService = new ListGamesService(authDAO, gameDAO);
+        createGameService = new CreateGameService(authDAO, gameDAO);
+        joinGameService = new JoinGameService(authDAO,gameDAO);
+        clearService = new ClearService(authDAO, gameDAO, userDAO);
+
+        clearService.clear();
+
+    }
+
+    @Test
+    public void registerTest() throws DataAccessException {
+        RegisterRequest request = new RegisterRequest("jocelynp", "jocelyn", "jocelynperucca@gmail.com");
+        RegisterResult result = registerService.register(request);
+        Assertions.assertEquals("created", result.message());
+    }
+
+
 }
