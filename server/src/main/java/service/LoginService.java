0660caa13935849aac8361f131aad9c82683b7f3
jocelynperucca <jocelynperucca@gmail.com>
2024-10-21 13:53:22 -0600

    added comments and deleted empty lines

--- server/src/main/java/service/LoginService.java
+++ server/src/main/java/service/LoginService.java
@@ -7,9 +7,9 @@ import model.AuthData;
 import requestsAndResults.LoginRequest;
 import requestsAndResults.LoginResult;
 import model.UserData;
-
 import java.util.UUID;
 
+//Handles user authentication by verifying credentials and generating an authorization token upon successful login.
 public class LoginService {
 
     private final UserDAO userDao;
@@ -20,10 +20,12 @@ public class LoginService {
         this.authDao = authDao;
     }
 
+    // Generates a random UUID string for an authToken
     public static String generateAuthToken() {
-        return UUID.randomUUID().toString(); // Generates a random UUID string
+        return UUID.randomUUID().toString();
     }
 
+    //Handles login requests
     public LoginResult login(LoginRequest loginRequest) throws DataAccessException {
         String username = loginRequest.username();
         String password = loginRequest.password();
@@ -31,18 +33,17 @@ public class LoginService {
 
         if(userData == null) {
             return new LoginResult(null, null, "Error: unauthorized");
+
         } else {
             if (userDao.verifyPassword(userData, password) == null) {
                 return new LoginResult(null, null, "Error: unauthorized");
+
             } else {
                 String authToken = generateAuthToken();
                 AuthData authUserData = new AuthData(username, authToken);
                 authDao.saveAuthToken(authUserData);
                 return new LoginResult(username, authToken, "Logged In");
-
             }
         }
-
     }
-
 }
