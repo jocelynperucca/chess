2995d59b385fa1d5fd8a3b05dfc4409a9dd1ab04
jocelynperucca <jocelynperucca@gmail.com>
2024-10-21 12:08:44 -0600

    updated comments and deletion of dead code

--- server/src/main/java/server/Handler/LogoutHandler.java
+++ server/src/main/java/server/Handler/LogoutHandler.java
@@ -8,21 +8,25 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 
+// Handles HTTP requests for user logout, invoking LogoutService and setting the appropriate response status.
 public class LogoutHandler implements Route {
 
     private final LogoutService logoutService;
-    private final Gson gson = new Gson(); // Initialize Gson here
+    private final Gson gson = new Gson();
 
     public LogoutHandler(LogoutService logoutService) {
         this.logoutService = logoutService;
     }
 
     @Override
-    public Object handle(Request Request, Response response) throws DataAccessException {
+    public Object handle(Request request, Response response) {
 
+        //Initialize logoutResult to be updated as depending on the message
         LogoutResult logoutResult;
+
+        // Handle logout request, set appropriate HTTP status based on service result
         try {
-            String authToken = Request.headers("Authorization");
+            String authToken = request.headers("Authorization");
             logoutResult = logoutService.logout(authToken);
             if (logoutResult.message().contains("Logged Out")) {
                 response.status(200);
@@ -30,18 +34,11 @@ public class LogoutHandler implements Route {
                 response.status(401);
             }
 
-
         } catch (DataAccessException e) {
             logoutResult = new  LogoutResult(e.getMessage());
             response.status(500);
-
-            //throw new RuntimeException(e);
-
         }
 
-        response.type("application/json");
         return gson.toJson(logoutResult);
-
-        //return logoutUser(authToken, response); // Call the refactored method
     }
 }
