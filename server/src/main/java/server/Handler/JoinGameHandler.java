66c45cbf348daece35239b4b1e8dd19ec09b25ee
jocelynperucca <jocelynperucca@gmail.com>
2024-10-21 11:25:28 -0600

    deleted unneeded comments

--- server/src/main/java/server/Handler/JoinGameHandler.java
+++ server/src/main/java/server/Handler/JoinGameHandler.java
@@ -9,11 +9,11 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 
-
+// Handles HTTP requests to join a game, invoking JoinGameService and setting the appropriate response status.
 public class JoinGameHandler implements Route {
 
     private final JoinGameService joinGameService;
-    private final Gson gson = new Gson(); // Initialize Gson here
+    private final Gson gson = new Gson();
 
     public JoinGameHandler(JoinGameService joinGameService) {
         this.joinGameService = joinGameService;
@@ -21,20 +21,21 @@ public class JoinGameHandler implements Route {
 
     @Override
     public Object handle(Request request, Response response) throws DataAccessException {
-        return joinGame(request, response); // Call the refactored method
+        return joinGame(request, response);
     }
 
     private String joinGame(Request req, Response res) throws DataAccessException {
-        // Parse the request body to RegisterRequest object
-        JoinGameRequest joinGameRequest = gson.fromJson(req.body(), JoinGameRequest.class);
 
+        // Parse the request body to joinGameRequest object
+        JoinGameRequest joinGameRequest = gson.fromJson(req.body(), JoinGameRequest.class);
         String authToken = req.headers("Authorization");
 
+        //Initialize result to be read HTTP status
         JoinGameResult result = joinGameService.joinGame(joinGameRequest, authToken);
 
         // Set the appropriate HTTP status based on the result message
         if (result.message().equals("Joined Game")) {
-            res.status(200); // Success
+            res.status(200);
         } else if (result.message().contains("unauthorized")) {
             res.status(401);
         } else if (result.message().contains("bad request")) {
@@ -42,10 +43,10 @@ public class JoinGameHandler implements Route {
         } else if (result.message().contains("already taken")) {
             res.status(403);
         } else {
-            res.status(500); // Internal server error or unexpected case
+            res.status(500);
         }
 
-        // Return the RegisterResult as a JSON string
+        // Return the result
         return gson.toJson(result);
     }
 }
