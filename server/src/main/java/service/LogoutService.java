1fe63cb88f52d82eee9e8ce1da057fb7f9f02a56
jocelynperucca <jocelynperucca@gmail.com>
2024-10-21 13:57:38 -0600

    added comments and deleted empty lines

--- server/src/main/java/service/LogoutService.java
+++ server/src/main/java/service/LogoutService.java
@@ -4,6 +4,8 @@ import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
 import requestsAndResults.LogoutResult;
 
+
+//Manages user logout functionality by validating the authorization token and removing it from the data store upon successful logout.
 public class LogoutService {
 
     private final AuthDAO authDao;
@@ -13,12 +15,13 @@ public class LogoutService {
     }
 
     public LogoutResult logout(String authToken) throws DataAccessException {
+
         if (authDao.getAuthToken(authToken) == null) {
             return new LogoutResult("Error: unauthorized");
+
         } else {
             authDao.deleteAuthToken(authToken);
             return new LogoutResult("Logged Out");
         }
-
     }
 }
