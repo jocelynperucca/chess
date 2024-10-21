5de8ad3970097f0d530af289884d8cc3a6f05aea
jocelynperucca <jocelynperucca@gmail.com>
2024-10-21 13:47:08 -0600

    added comments and deleted empty lines

--- server/src/main/java/service/JoinGameService.java
+++ server/src/main/java/service/JoinGameService.java
@@ -7,6 +7,8 @@ import model.GameData;
 import requestsAndResults.JoinGameRequest;
 import requestsAndResults.JoinGameResult;
 
+
+//Handles game joining logic, verifying authorization and player color, while updating game data accordingly.
 public class JoinGameService {
 
     private final AuthDAO authDao;
@@ -17,42 +19,45 @@ public class JoinGameService {
         this.gameDao = gameDao;
     }
 
+    //function to attempt to join a game based on color and authorization
     public JoinGameResult joinGame(JoinGameRequest joinGameRequest, String authToken) throws DataAccessException {
         if (authDao.getAuthToken(authToken) == null) {
             return new JoinGameResult("Error: unauthorized");
         }
 
+        //get GameData
         int joinGameID = joinGameRequest.gameID();
         GameData gameData = gameDao.findGame(joinGameID);
-
         JoinGameResult joinGameResult;
+
         if (gameData == null) {
             return new JoinGameResult("Error: bad request");
         } else {
+            //get requested color and username to join
             String playerColor = joinGameRequest.playerColor();
             String user = authDao.getAuthToken(authToken).getUsername();
             joinGameResult = new JoinGameResult("");
 
-            if(playerColor == null) {
+            if (playerColor == null || (!playerColor.equals("BLACK") && !playerColor.equals("WHITE"))) {
                 joinGameResult = new JoinGameResult("Error: bad request");
-            }
 
-            else if (playerColor.equalsIgnoreCase("WHITE") && gameData.getWhiteUsername() != null) {
+            } else if (playerColor.equalsIgnoreCase("WHITE") && gameData.getWhiteUsername() != null) {
                 joinGameResult = new JoinGameResult("Error: already taken");
+
             } else if (playerColor.equalsIgnoreCase("BLACK") && gameData.getBlackUsername() != null) {
                 joinGameResult = new JoinGameResult("Error: already taken");
+
             } else if (playerColor.equalsIgnoreCase("WHITE")) {
                 gameDao.updateGameData(joinGameID, "WHITE", user);
                 joinGameResult = new JoinGameResult("Joined Game");
+
             } else if (playerColor.equalsIgnoreCase("BLACK")) {
                 gameDao.updateGameData(joinGameID, "BLACK", user);
                 joinGameResult = new JoinGameResult("Joined Game");
-            } else if (playerColor == null || playerColor != "BLACK" || playerColor != "WHITE"){
-                joinGameResult = new JoinGameResult("Error: bad request");
             }
-
         }
-        return joinGameResult;
 
+        //return message
+        return joinGameResult;
     }
 }
