d5cdfebdd610c55f7e9495343ad1fe25510f8a50
jocelynperucca <jocelynperucca@gmail.com>
2024-10-21 11:29:10 -0600

    updated comments and deletion of dead code

--- server/src/main/java/server/Handler/ListGamesHandler.java
+++ server/src/main/java/server/Handler/ListGamesHandler.java
@@ -8,35 +8,39 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 
+// Handles HTTP requests to list available games, invoking ListGamesService and setting the response status.
 public class ListGamesHandler implements Route {
 
     private final ListGamesService listGamesService;
-    private final Gson gson = new Gson(); // Initialize Gson here
+    private final Gson gson = new Gson();
 
     public ListGamesHandler(ListGamesService listGamesService) {
         this.listGamesService = listGamesService;
     }
 
     @Override
-    public Object handle(Request sparkRequest, Response response) throws DataAccessException {
+    public Object handle(Request request, Response response) {
 
+        //Initialize listGamesResult to be changed based on message
         ListGamesResult listGamesResult;
 
+        // Handle request to list games, set appropriate status based on authorization and service result
         try {
-            String authToken = sparkRequest.headers("Authorization");
+            String authToken = request.headers("Authorization");
             listGamesResult = listGamesService.listGames(authToken);
+
             if (listGamesResult.message().contains("Listed Games")) {
                 response.status(200);
             } else if (listGamesResult.message().contains("unauthorized")) {
                 response.status(401);
             }
+
         } catch (DataAccessException e) {
             listGamesResult = new ListGamesResult(null, e.getMessage());
             response.status(500);
-
         }
 
-        response.type("application/json");
+        //Return result and status
         return gson.toJson(listGamesResult);
     }
 }
