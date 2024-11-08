b83ed9ab7334088850592f349f3874d8fe435703
jocelynperucca <jocelynperucca@gmail.com>
2024-11-08 15:09:55 -0700

    to create menus

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,4 +1,41 @@
 package ui;
 
+import org.eclipse.jetty.util.Scanner;
+
+import java.io.PrintStream;
+import java.util.Arrays;
+
 public class ChessClient {
+
+    private final PrintStream out;
+
+    public ChessClient(String serverUrl) {
+        this.out = new PrintStream(System.out, true);
+    }
+
+    public void beforeLogin(PrintStream out) {
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens [0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                default -> help();
+            }
+        }
+    }
+
+    public String help() {
+        if (state == State.SIGNEDOUT) {
+            return """
+                    - signIn <USERNAME> <PASSWORD>
+                    -
+                    """;
+        }
+        return """
+                
+                """;
+    }
 }
