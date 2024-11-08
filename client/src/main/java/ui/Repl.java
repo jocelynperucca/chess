d32fc558aeafb2713c73b5eaf6d797f4f774617b
jocelynperucca <jocelynperucca@gmail.com>
2024-11-07 17:43:56 -0700

    Creation of ChessClient to create menus

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -4,4 +4,9 @@ public class Repl {
     public Repl(String serverUrl) {
         new ServerFacade(serverUrl);
     }
+
+    public void run() {
+        System.out.println("Welcome to Chess!");
+        System.out.print
+    }
 }
