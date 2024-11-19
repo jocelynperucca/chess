6dd88b24969cf2fba11aff7007a2641093dfb34e
jocelynperucca <jocelynperucca@gmail.com>
2024-11-19 14:57:09 -0700

    Start to ErrorMessage I think

--- shared/src/main/java/websocket/messages/ErrorMessage.java
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -1,4 +1,17 @@
 package websocket.messages;
 
-public class ErrorMessage {
+public class ErrorMessage extends ServerMessage {
+    private final String errorMessage;
+
+    public ErrorMessage(String errorMessage) {
+        super(ServerMessageType.ERROR);
+        if(!errorMessage.contains("Error")) {
+            throw new IllegalArgumentException("ErrorMessage must contain 'Error'");
+        }
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
 }
