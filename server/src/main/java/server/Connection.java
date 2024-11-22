b4e703dba98588145d813228f1e628b2e0c0b256
jocelynperucca <jocelynperucca@gmail.com>
2024-11-22 14:51:25 -0700

    petshop connection

--- server/src/main/java/server/Connection.java
+++ server/src/main/java/server/Connection.java
@@ -1,4 +1,18 @@
 package server;
+import java.io.IOException;
+import org.eclipse.jetty.websocket.api.Session;
 
 public class Connection {
+    public String visitorName;
+    public Session session;
+
+    public Connection(String visitorName, Session session) {
+        this.visitorName = visitorName;
+        this.session = session;
+    }
+
+    public void send(String msg) throws IOException {
+        session.getRemote().sendString(msg);
+    }
+
 }
