3d4fb801c15c2e26412477b668ed7d27b917468c
jocelynperucca <jocelynperucca@gmail.com>
2024-11-21 19:01:33 -0700

    Start of WebSocketFacade from petshop

---
+++ client/src/main/java/WebSocket/WebSocketFacade.java
@@ -0,0 +1,43 @@
+package WebSocket;
+
+import com.sun.nio.sctp.NotificationHandler;
+import ui.ResponseException;
+import com.google.gson.Gson;
+import websocket.messages.NotificationMessage;
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class WebSocketFacade extends Endpoint {
+
+    Session session;
+    NotificationHandler notificationHandler;
+
+    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws ResponseException {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+            this.notificationHandler = notificationHandler;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            //set message handler
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    NotificationMessage notification = new Gson().fromJson(message, NotificationMessage.class);
+                    notificationHandler.notify();
+                }
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+
+    }
+}
